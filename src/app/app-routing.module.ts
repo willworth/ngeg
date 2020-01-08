12ebb85f08ed9e1ca6965a668267b51de167d89f
will worth <will.worth@gmail.com>
2020-01-08 14:45:35 +0100

    routing and style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloworldComponent } from './helloworld/helloworld.component';
+import { FormexampleComponent } from './formexample/formexample.component'
 
+const routes: Routes = [
+  { path: 'home', component: HelloworldComponent},
+  { path: 'forms', component: FormexampleComponent},
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
